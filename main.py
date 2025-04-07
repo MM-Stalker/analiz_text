7646643aee46ebcc4a7e679139fb9df8c3b91c07
MM-Stalker <leoniddubik169@gmail.com>
2025-04-07 19:33:30 +0300

    Add loop

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from spacy.lang.ru import Russian
 from spacy.lang.ru.stop_words import STOP_WORDS
 
 model = Russian()
+
+text = model("Фильм так себе,слабый сюжет и актерская игра просто ужасна")
+print(text)
+text_list = [word for word in text]
+print(text_list)
